aa84f7f234c44c7a4dd500249014bd671ab7ff58
Stanislav Nikolov <valsinats@gmail.com>
2015-08-19 23:18:49 +0100

    Add a Material style Toolbar

--- mobile/src/main/java/me/stanislav_nikolov/meditate/MainActivity.java
+++ mobile/src/main/java/me/stanislav_nikolov/meditate/MainActivity.java
@@ -1,16 +1,22 @@
 package me.stanislav_nikolov.meditate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = ((Toolbar) findViewById(R.id.toolbar));
+        setSupportActionBar(toolbar);
     }
 
     @Override
